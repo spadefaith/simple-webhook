393b70d3c0718cdd7901f2047ea3d017ccf83673
cedrick <cedrickcampoto@yahoo.com>
2022-11-24 09:18:23 +0800

    update

--- services/TriggerBash.js
+++ services/TriggerBash.js
@@ -17,10 +17,15 @@ module.exports = function(script, callbackLog){
 
           child.stdout.on('data', async (data) => {
             data = encodeURI(data.toString());
+
+            console.log(data);
             await callbackLog(data);
           });
           child.stderr.on('data', async (data) => {
-            await callbackLog(data.toString());
+            data = encodeURI(data.toString());
+
+            console.log(data);
+            await callbackLog(data);
           });
           child.on('close', async (code) => {
               console.log(19,`child process exited with code ${code}`);
