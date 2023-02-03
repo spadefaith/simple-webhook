f9a5a1bc53a5dedf6079a0afa223e0540c3a31fd
cedrick <cedrickcampoto@yahoo.com>
2023-02-04 04:46:44 +0800

    add deploy script

--- services/ReplaceVariable.js
+++ services/ReplaceVariable.js
@@ -5,7 +5,7 @@ module.exports = function(strings, options){
     // console.log(5,options);
     return strings.map(string=>{
         return Object.keys(options).reduce((accu,key)=>{
-            let value = `"${options[key]}"`;
+            let value = options[key];
             key = `$${key}`;
             accu.includes(key) && (accu = accu.replace(key,value || "_"));
             return accu;
