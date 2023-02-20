24a475d8881f7bac949119f8f54b645687d6abc7
cedrick <cedrickcampoto@yahoo.com>
2023-02-20 20:52:07 +0800

    add slack notifier

--- services/index.js
+++ services/index.js
@@ -1,4 +1,5 @@
 exports.NotifyTelegram = require('./NotifyTelegram');
+exports.NotifySlack = require('./NotifySlack');
 exports.TriggerBash = require('./TriggerBash');
 exports.TriggerHttp = require('./TriggerHttp');
 exports.ReplaceVariable = require('./ReplaceVariable');
