58dc762130dce15de7275d2a7753ea3b8ed8bf15
cedrick <cedrickcampoto@yahoo.com>
2023-02-20 20:58:37 +0800

    add name in bitbucket

--- utils/ParseBitbucket.js
+++ utils/ParseBitbucket.js
@@ -1,10 +1,10 @@
 
 module.exports = function(json){
     
-    let {push:{changes:[{new:{name:branch, target:{message}}}]}} = json;
+    let {push:{changes:[{new:{name:branch, target:{message}}}]},actor:{display_name:name}} = json;
 
 
     console.log(7,json);
 
-    return {branch,message};
+    return {branch,message,name};
 }
